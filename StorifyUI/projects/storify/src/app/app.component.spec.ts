8f96c21f4300279b28dbb53e1fce3f8e09d7d8de
unknown <Administrator@MAshraf_IMP.HealthInsightsEG.local>
2023-10-11 05:25:45 +0300

    new layout

--- StorifyUI/projects/storify/src/app/app.component.spec.ts
+++ StorifyUI/projects/storify/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MaterialSetup'`, () => {
+  it(`should have as title 'Storify'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MaterialSetup');
+    expect(app.title).toEqual('Storify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MaterialSetup app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Storify app is running!');
   });
 });
